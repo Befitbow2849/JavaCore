4795ca026b33e2db7b17d7a56490601e66013f11
Vladislav Kulgin <taksa_@mail.ru>
2021-02-17 19:45:31 +0300

    add example

---
+++ src/main/java/com/Befitbow2849/javacore/chapter1/Example.java
@@ -0,0 +1,10 @@
+package com.Befitbow2849.javacore.chapter1;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Simple java programm");
+    }
+
+
+
+}
