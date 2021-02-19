9e9cd81a5cf9f61be682bcd17770236ec56b17a7
Vladislav Kulgin <taksa_@mail.ru>
2021-02-19 19:47:48 +0300

    add example  ArrayAverage

---
+++ src/main/java/com/Befitbow2849/javacore/chapter3/Average.java
@@ -0,0 +1,13 @@
+package com.Befitbow2849.javacore.chapter3;
+
+public class Average {
+    public static void main(String[] args) {
+        double nums[] = {10.1, 11.2, 12.3 , 13.4, 14.5};
+        double result = 0;
+        int i = 0;
+        for (i=0; i<5; i++){
+            result = result + nums[i];
+            System.out.println("Среднее значение равно " + result/5);
+        }
+    }
+}
