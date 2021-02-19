5e6dc5a46526a59d725b430d29006724978279db
Vladislav Kulgin <taksa_@mail.ru>
2021-02-19 19:56:15 +0300

    add example  TwoDArray

---
+++ src/main/java/com/Befitbow2849/javacore/chapter3/TwoDArray.java
@@ -0,0 +1,26 @@
+package com.Befitbow2849.javacore.chapter3;
+
+import java.sql.SQLOutput;
+
+public class TwoDArray {
+    public static void main(String[] args) {
+        int twoD[][] = new int [4][5];
+        int k = 0;
+        for (int i = 0; i<4; i++){
+            for (int j=0; j<5; j++){
+                twoD[i][j] = k;
+            k++;
+            }
+
+        }
+        for (int i = 0; i <4; i++){
+            for (int j = 0; j<5; j++)
+                System.out.print(twoD[i][j] + " ");
+            System.out.println();
+
+        }
+
+
+
+    }
+}
