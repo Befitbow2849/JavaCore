ce2420d47d6a4179dcfd6932adfdbbdd4f4fda70
Vladislav Kulgin <taksa_@mail.ru>
2021-02-20 20:03:05 +0300

    add example SimpleSwitch

---
+++ src/main/java/com/Befitbow2849/javacore/chapter5/SimpleSwitch.java
@@ -0,0 +1,30 @@
+package com.Befitbow2849.javacore.chapter5;
+
+public class SimpleSwitch {
+    public static void main(String[] args) {
+        for (int i =0; i<= 6; i ++)
+            switch(i){
+                case 0:
+                    System.out.println(i);
+                    break;
+                case 1:
+                    System.out.println(i);
+                    break;
+                case 2:
+                    System.out.println(i);
+                    break;
+                case 3:
+                    System.out.println(i);
+                    break;
+                case 4:
+                    System.out.println(i);
+                    break;
+                case 5:
+                    System.out.println(i);
+                    break;
+                default:
+                    System.out.println(6);
+
+            }
+    }
+}
