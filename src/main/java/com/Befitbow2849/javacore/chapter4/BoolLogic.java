2d46a762f0662d46952286d67587685e1197c78f
Vladislav Kulgin <taksa_@mail.ru>
2021-02-20 18:42:18 +0300

    add example BoolLogic

---
+++ src/main/java/com/Befitbow2849/javacore/chapter4/BoolLogic.java
@@ -0,0 +1,18 @@
+package com.Befitbow2849.javacore.chapter4;
+
+public class BoolLogic {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        boolean c = a | b;
+        boolean d = a & b;
+        boolean f = (!a & b) | (a & !b);
+        boolean g = !a;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(g);
+    }
+}
