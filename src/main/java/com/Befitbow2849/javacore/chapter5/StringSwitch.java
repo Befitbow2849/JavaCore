7418b8385051fb3d7b412717c066f516c7bf1201
Vladislav Kulgin <taksa_@mail.ru>
2021-02-20 20:42:57 +0300

    add example Switch

---
+++ src/main/java/com/Befitbow2849/javacore/chapter5/StringSwitch.java
@@ -0,0 +1,24 @@
+package com.Befitbow2849.javacore.chapter5;
+
+public class StringSwitch {
+    public static void main(String[] args) {
+        String str = "два";
+        switch (str){
+            case "один":
+                System.out.println("Один");
+                break;
+            case "два":
+                System.out.println("Два");
+                break;
+            case "три":
+                System.out.println("Три");
+                break;
+            default:
+                System.out.println("Несовпало");
+
+
+
+
+        }
+    }
+}
