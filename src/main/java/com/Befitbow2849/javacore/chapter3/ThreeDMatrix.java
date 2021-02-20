e5bc0752322374a88413a83cd9feeee1aff16d79
Vladislav Kulgin <taksa_@mail.ru>
2021-02-20 17:44:35 +0300

    add example  Matrix3d

---
+++ src/main/java/com/Befitbow2849/javacore/chapter3/ThreeDMatrix.java
@@ -0,0 +1,18 @@
+package com.Befitbow2849.javacore.chapter3;
+
+public class ThreeDMatrix {
+    public static void main(String[] args) {
+            int threed[][][] = new int[3][4][5];
+            for (int i = 0; i<3; i++){
+                System.out.println();
+                for (int j = 0; j<4; j++){
+                    for (int k = 0; k<5; k++) {
+                        threed[i][j][k] = i*j*k;
+                        System.out.print(threed[i][j][k] +  " ");
+                    }
+                    System.out.println();
+                }
+            }
+
+    }
+}
