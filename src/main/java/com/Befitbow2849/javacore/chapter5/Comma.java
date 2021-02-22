476815158b7d6eceed94ce25bdab4c721acad359
Vladislav Kulgin <taksa_@mail.ru>
2021-02-22 22:20:11 +0300

    add example Comma!!!!

---
+++ src/main/java/com/Befitbow2849/javacore/chapter5/Comma.java
@@ -0,0 +1,11 @@
+package com.Befitbow2849.javacore.chapter5;
+
+public class Comma {
+    public static void main(String[] args) {
+        int a,b;
+        for (a=1, b=4; a<4; a++,b-- ){
+            System.out.println("a");
+            System.out.println("b");
+        }
+    }
+}
