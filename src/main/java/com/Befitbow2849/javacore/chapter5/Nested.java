82ad6d793243fca5a1650d97eb7b0c386f978afd
Vladislav Kulgin <taksa_@mail.ru>
2021-02-23 00:16:00 +0300

    add example Searh

---
+++ src/main/java/com/Befitbow2849/javacore/chapter5/Nested.java
@@ -0,0 +1,13 @@
+package com.Befitbow2849.javacore.chapter5;
+
+public class Nested {
+    public static void main(String[] args) {
+        int i=0, j=0;
+        for (; i<10; i++){
+            for( ; j<10; j++){
+                System.out.println(".");
+            }
+            System.out.println();
+        }
+    }
+}
