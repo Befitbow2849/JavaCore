1ab574772c53625217034cbaaf85e38bec18a2ff
Vladislav Kulgin <taksa_@mail.ru>
2021-02-22 23:09:54 +0300

    add example ForEach2

---
+++ src/main/java/com/Befitbow2849/javacore/chapter5/ForEach2.java
@@ -0,0 +1,13 @@
+package com.Befitbow2849.javacore.chapter5;
+
+public class ForEach2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int nums[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int x: nums){
+            sum+=x;
+            if (x==5)break;
+        }
+        System.out.println("Сумма пяти первых элементов равна: " + sum);
+    }
+}
