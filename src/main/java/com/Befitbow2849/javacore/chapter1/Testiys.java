a70750be6408df0264f0820e3373417303fb246b
Vladislav Kulgin <taksa_@mail.ru>
2021-02-23 21:14:12 +0300

    add example Continue

---
+++ src/main/java/com/Befitbow2849/javacore/chapter1/Testiys.java
@@ -0,0 +1,12 @@
+package com.Befitbow2849.javacore.chapter1;
+
+public class Testiys {
+    public static void main(String[] args) {
+        int mass[] = new int[50];
+        for (int i =50; i>=1; i --){
+
+            mass[i-1]= i * 2-1;
+            System.out.println(mass[i-1]);
+        }
+    }
+}
