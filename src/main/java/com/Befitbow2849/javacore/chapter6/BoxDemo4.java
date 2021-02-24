5a93d5ce58216de0a3c184f375ac28f393a3863c
Vladislav Kulgin <taksa_@mail.ru>
2021-02-24 19:28:39 +0300

    add example BoxDemo4

---
+++ src/main/java/com/Befitbow2849/javacore/chapter6/BoxDemo4.java
@@ -0,0 +1,29 @@
+package com.Befitbow2849.javacore.chapter6;
+
+class Box4{
+    double width;
+    double height;
+    double depth;
+    double volume (){
+        return width * height * depth;
+    }
+}
+
+public class BoxDemo4 {
+    public static void main(String[] args) {
+        double vol;
+        Box4 mybox1 = new Box4();
+        Box4 mybox2 = new Box4();
+        mybox1.width = 10;
+        mybox1.height = 20;
+        mybox1.depth = 15;
+        mybox2.depth = 9;
+        mybox2.height = 6;
+        mybox2.width = 3;
+
+        vol = mybox1.volume();
+        System.out.println(vol + " объем первого");
+        vol = mybox2.volume();
+        System.out.println(vol + " объем второго");
+    }
+}
