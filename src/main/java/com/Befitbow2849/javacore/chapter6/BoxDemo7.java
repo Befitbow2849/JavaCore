ba186fe49447ef1fa5ac71e02596d1bd330baf6a
Vladislav Kulgin <taksa_@mail.ru>
2021-02-25 17:28:36 +0300

    add example BoxDemo7

---
+++ src/main/java/com/Befitbow2849/javacore/chapter6/BoxDemo7.java
@@ -0,0 +1,32 @@
+package com.Befitbow2849.javacore.chapter6;
+class Box7{
+
+    double width;
+    double height;
+    double depth;
+
+    Box7(double w, double h, double d){
+        width = w;
+        height = h;
+        depth = d;
+
+
+    }
+
+    double volume (){
+        return width * height * depth;
+    }
+
+}
+
+public class BoxDemo7 {
+    public static void main(String[] args) {
+        Box7 mybox1 = new Box7(10,20,30);
+        Box7 mybox2 = new Box7(20,30,40);
+        double val = mybox1.volume();
+        System.out.println(val);
+        val = mybox2.volume();
+        System.out.println(val);
+
+    }
+}
