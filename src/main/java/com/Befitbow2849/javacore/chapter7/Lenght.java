258220f28b977a721407baffaaa3d50149464efa
Vladislav Kulgin <taksa_@mail.ru>
2021-02-27 18:41:17 +0300

    add example Lenght

---
+++ src/main/java/com/Befitbow2849/javacore/chapter7/Lenght.java
@@ -0,0 +1,12 @@
+package com.Befitbow2849.javacore.chapter7;
+
+public class Lenght {
+    public static void main(String[] args) {
+        int a1[] = new int[10];
+        int a2[] = {3, 4, 5, 6, 99, -10};
+        int a3[] = {4, 3, 2, 1};
+        System.out.println("Длинна a1 = " + a1.length);
+        System.out.println("Длинна а2 = " + a2.length);
+        System.out.println("Длинна а3 = " + a3.length);
+    }
+}
