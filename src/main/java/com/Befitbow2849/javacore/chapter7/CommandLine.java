96296238854d3ecb6a3ca8601389690b3098abe3
Vladislav Kulgin <taksa_@mail.ru>
2021-02-28 18:02:11 +0300

    add example CommandLine

---
+++ src/main/java/com/Befitbow2849/javacore/chapter7/CommandLine.java
@@ -0,0 +1,8 @@
+package com.Befitbow2849.javacore.chapter7;
+
+public class CommandLine {
+    public static void main(String[] args) {
+        for (int i = 0; i <args.length; i ++)
+            System.out.println(args[i]);
+    }
+}
